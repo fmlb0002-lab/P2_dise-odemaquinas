7ad6e8a25165b4bb5e06979bd55749d32811308a
fmlb0002-lab <fmlb0002@red.ujaen.es>
2026-02-27 21:31:39 +0100

    Update P2_ConcentradorTension_v2.m

--- P2_ConcentradorTension_v2.m
+++ P2_ConcentradorTension_v2.m
@@ -26,14 +26,15 @@ end
 % --------- Procesado + export ----------
 % 1) EntallaLateral (PC)
 stress_EntLat = lame_stress(datos.EntallaLateral.S, mat.PC);
-%export_maps_sigma_x(stress_EntLat, "maps_EntallaLateral_x", [0 3.5e6]);
+export_maps_sigma_x(stress_EntLat, "maps_EntallaLateral_x", [0 3.5e6]);
 
 % 2) OrificioCentral (PC)
 stress_OrCent = lame_stress(datos.OrificioCentral.S, mat.PC);
-%export_maps_sigma_x(stress_OrCent, "maps_OrificioCentral_x", [0 3.5e6]);
+export_maps_sigma_x(stress_OrCent, "maps_OrificioCentral_x", [0 3.5e6]);
 
 % 3) OrificioFA (PLA)
 stress_OrFA = lame_stress(datos.OrificioFA.S, mat.PLA);
-%export_maps_sigma_x(stress_OrFA, "maps_OrificioFA_x", [0 5e6]);
+export_maps_sigma_x(stress_OrFA, "maps_OrificioFA_x", [0 5e6]);
+
 
 
